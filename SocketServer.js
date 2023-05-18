2a8560f293f7608ecf7e430c5d38d7a3bac2ace4
parzival1405 <80945839+parzival1405@users.noreply.github.com>
2023-05-18 22:13:19 +0700

    new func

--- SocketServer.js
+++ SocketServer.js
@@ -15,13 +15,9 @@ const SocketServer = (socket, query) => {
       id: query.id,
       role: query.role,
       socketId: socket.id,
+      idDepartment: query.idDepartment
     });
   }
-
-  socket.on("disconnect", () => {
-    users = users.filter((user) => user.socketId !== socket.id);
-    console.log("hre")
-  });
 
   socket.on("newMedicalExamination", (data) => {
     const data2 = JSON.parse(data);
@@ -42,7 +38,7 @@ const SocketServer = (socket, query) => {
 
   socket.on("doneServiceCLS", (data) => {
     const data2 = JSON.parse(data);
-    const client = users.find((user) => user.id === data2.va.doctorId+ "");
+    const client = users.find((user) => user.id === data2.va.doctorId + "");
 
     if (client) {
       socket.to(`${client.socketId}`).emit("receiveDoneServiceCLS", {
@@ -76,11 +72,49 @@ const SocketServer = (socket, query) => {
   socket.on("receptionistSendMessage", (data) => {
     const data2 = JSON.parse(data);
 
-    const client = users.find((user) => user.id === data2.customerId && user.role === "KHACH_HANG");
-    console.log(client,data2)
+    const client = users.find(
+      (user) => user.id === data2.customerId && user.role === "KHACH_HANG"
+    );
+    console.log(client, data2);
     if (client) {
       socket.to(`${client.socketId}`).emit("customerReceiveMessage", data2);
     }
+  });
+
+  socket.on("checkDoctorOnline", (idDepartment) => {
+    let doctors = users.filter((user) => user.role === "DOCTOR" && user.idDepartment === idDepartment+"");
+
+    socket.emit("numberTofDoctorOnlineToMe", doctors.length);
+  });
+
+  let doctors2 = users.filter((user) =>
+    user.idDepartment === query.idDepartment
+  );
+
+  if (doctors2.length > 0) {
+    doctors2.forEach((client) => {
+      socket
+        .to(`${client.socketId}`)
+        .emit("checkUserOnlineToClient", doctors2.length);
+    });
+  }
+
+  socket.on("disconnect", () => {
+    const offUser = users.find((user) => user.socketId == socket.id);
+    console.log(offUser)
+    if (offUser) {
+      const remain = users.filter((user) => user.socketId != socket.id && user.role === "DOCTOR");
+      console.log(remain)
+      if (remain.length > 0) {
+        remain.forEach((client) => {
+          if (client) {
+            socket.to(`${client.socketId}`).emit("CheckUserOfflineToClient", remain.length);
+          }
+        });
+      }
+    }
+
+    users = users.filter((user) => user.socketId !== socket.id);
   });
 };
 
